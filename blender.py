dc6d7495039c39f5549b0c48b7178c7a41993dfa
matheusfillipe <matheusfillipeag@gmail.com>
2022-07-03 16:45:20 -0300

    Updates on blender exporting

--- blender.py
+++ blender.py
@@ -1,11 +1,12 @@
+# Blender script to generate the road mesh from stl model
+
 import json
 import sys
 import tempfile
-from math import radians, sqrt
+from math import radians, sqrt, pi
 from pathlib import Path
 
 import bpy
-from mathutils import Vector
 
 argv = sys.argv
 argv = argv[argv.index("--") + 1:]
@@ -54,14 +55,16 @@ print("script: Running Script...")
 bpy.context.view_layer.active_layer_collection = (
     bpy.context.view_layer.layer_collection.children[0]
 )
-filepath = argv[0]  # "/home/matheus/tmp/testhighway.stl"
-# dxf="/home/matheus/Downloads/Telegram Desktop/highway.dxf"
-# start= [728352.9266,7787532.9365,484.0]
+
+# .stl trimesh filepath
+filepath = argv[0]
+
 bpy.ops.preferences.addon_enable(module="io_import_dxf")
 bpy.ops.preferences.addon_enable(module="io_export_dxf")
 jsonfile = str(Path(tempfile.gettempdir()) / "georoadBlender.json")
 with open(jsonfile, "r") as file:
     data = json.load(file)
+
 start = data["start"]
 points = data["points"]
 print("script: json points loaded")
@@ -90,14 +93,7 @@ for i, pt in enumerate(points):
         print("No vertex at index: ", i)
 toggle()
 bpy.ops.mesh.select_mode(type="EDGE")
-print("script: finished defining eixo!!")
-
-
-# bpy.ops.import_scene.dxf(filepath=dxf)
-# curve=find_by_name("curve")
-# p=curve.data.splines[0].points[0].co
-# v=curve.matrix_world @ p
-# start=[v.x,v.y,v.z]
+print("script: finished defining main axis!!")
 
 # apply materials
 road = bpy.data.materials.get("Road")
@@ -127,12 +123,6 @@ toggle()
 bpy.ops.object.select_all(action="DESELECT")
 
 # create path for camera new obj
-# bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1}, TRANSFORM_OT_translate={"value":(0, 0, 10), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, True), "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})
-# bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((0, 0, 0), (0, 0, 0), (0, 0, 0)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})
-
-# bpy.ops.mesh.separate(type="SELECTED")
-
-
 coords = [pt for i, pt in enumerate(points)]
 curveData = bpy.data.curves.new("myCurve", type="CURVE")
 curveData.dimensions = "3D"
@@ -158,12 +148,6 @@ path = curveOB
 path.select_set(True)
 
 # select path obj
-# for obj in bpy.context.selected_objects:
-#    if obj.type == 'MESH' and obj.name!=mesh.name:
-#        bpy.ops.object.select_all(action='DESELECT')
-#        bpy.context.view_layer.objects.active = obj
-#        obj.select_set(True)
-#        path=obj
 bpy.context.view_layer.objects.active = path
 path.select_set(True)
 bpy.ops.object.convert(target="CURVE")
@@ -184,9 +168,11 @@ bpy.ops.object.location_clear(clear_delta=False)
 bpy.ops.object.constraint_add(type="FOLLOW_PATH")
 bpy.context.object.constraints["Follow Path"].target = path
 override = {"constraint": cam.constraints["Follow Path"]}
-bpy.ops.constraint.followpath_path_animate(override, constraint="Follow Path")
-bpy.context.object.constraints["Follow Path"].use_curve_follow = True
-# bpy.ops.transform.rotate(value=1.48353, orient_axis='X', orient_type='LOCAL', orient_matrix=((0.926739, 0.375705, -5.16602e-09), (-0.373651, 0.921672, -0.104428), (-0.039234, 0.0967774, 0.994532)), orient_matrix_type='LOCAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+area = next((a for a in bpy.context.screen.areas if a.type == "VIEW_3D"), None)
+if area is not None:
+    bpy.ops.constraint.followpath_path_animate(override, constraint="Follow Path")
+    bpy.context.object.constraints["Follow Path"].use_curve_follow = True
+
 bpy.data.objects["Camera"].rotation_euler[0] = 1.309
 bpy.data.objects["Camera"].rotation_euler[1] = 0
 bpy.data.objects["Camera"].rotation_euler[2] = 0
@@ -198,43 +184,16 @@ bpy.context.view_layer.active_layer_collection = (
     bpy.context.view_layer.layer_collection.children[-1]
 )
 for pt, est, azi in zip(data["points"], data["estacas"], data["azi"]):
-    bpy.ops.object.text_add(enter_editmode=False,
-                            location=(pt[0], pt[1], pt[2] + 15))
+    bpy.ops.object.text_add(enter_editmode=False, location=(pt[0], pt[1], pt[2] + 15))
+    bpy.context.object.data.materials.append(bpy.data.materials.get("Label"))
     bpy.context.object.data.body = est
     bpy.context.active_object.name = est
     bpy.context.object.data.size = 7.5
-    ov = bpy.context.copy()
-    ov['area'] = [a for a in bpy.context.screen.areas if a.type == "VIEW_3D"][0]
-    bpy.ops.transform.rotate(
-        ov,
-        value=1.5708,
-        orient_axis="X",
-        orient_type="GLOBAL",
-        orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-        orient_matrix_type="GLOBAL",
-        constraint_axis=(True, False, False),
-        mirror=True,
-        use_proportional_edit=False,
-        proportional_edit_falloff="SMOOTH",
-        proportional_size=1,
-        use_proportional_connected=False,
-        use_proportional_projected=False,
-    )
-    bpy.ops.transform.rotate(
-        ov,
-        value=-radians(float(azi)),
-        orient_axis="Z",
-        orient_type="GLOBAL",
-        orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-        orient_matrix_type="GLOBAL",
-        constraint_axis=(True, False, False),
-        mirror=True,
-        use_proportional_edit=False,
-        proportional_edit_falloff="SMOOTH",
-        proportional_size=1,
-        use_proportional_connected=False,
-        use_proportional_projected=False,
-    )
+
+    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_VOLUME', center='MEDIAN')
+    bpy.context.object.rotation_euler[0] = pi / 2
+    bpy.context.object.rotation_euler[2] = -radians(float(azi))
+
 print("script: labels added")
 
 
@@ -248,7 +207,7 @@ mesh.select_set(True)
 print("script: mesh selected")
 
 
-# yellow center
+# yellow centerline
 toggle()
 bpy.ops.mesh.select_mode(type="VERT")
 bpy.ops.mesh.select_all(action="DESELECT")
@@ -270,10 +229,10 @@ mesh.data.materials.append(eixo)
 mesh.active_material_index = 2
 bpy.ops.object.material_slot_assign()
 toggle()
-print("script: eixo colored")
+print("script: colors applied to main axis")
 
 
-# rendered
+# set rendered
 my_areas = bpy.context.workspace.screens[0].areas
 my_shading = "RENDERED"  # 'WIREFRAME' 'SOLID' 'MATERIAL' 'RENDERED'
 for area in my_areas:
